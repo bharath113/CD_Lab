973c5fff09dea29abb9b076751aee154c28fdcd2
bharath113 <bandarubharath7@gmail.com>
2018-04-02 14:41:10 +0530

    Add files via upload

---
+++ ICG/codegen.c
@@ -0,0 +1,357 @@
+
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+extern int nestlvl;
+extern int func_no;
+extern int funcCall;
+extern int whilecame,ifcame,only_if,forfor;
+
+struct stac
+{
+char sta[100];
+int add;
+int nestl;
+int funcl;
+int updated;
+char ope[10];
+struct stac*left;
+struct stac*right;
+};
+
+void createlabel();
+void createlabelforend();
+void createlabelforin();
+void gotoforincre();
+void createlabelforincrement();
+void onlyfor(struct stac*);
+void createfun(char *);
+void onlyif(struct stac*);
+void createlabelforonlyif();
+void afteronlyif();
+void icgif(struct stac*);
+void icgwhile(struct stac*d);
+int searchforupdate(struct stac*);
+void storeinextrastack(struct stac *);
+struct stac* searchinextrastack(struct stac* ,int );
+
+int while_cnt=0;
+int z;
+int ifelse_cnt=0;
+int y;
+int x=0,g;
+int h;
+int whilopen[100];
+char wopen[100][100];
+char ifafter[100][100];
+char ifonly[100][100];
+char forin[100];
+char ifelse[100][100];
+char forend[100][100];
+char forincre[100];
+int adds=100;
+int fortp=0;
+int n=0;
+int forstart;
+int increstart[100];
+int f=0;
+struct stac *stack_list[100];
+
+struct stac *extra_stac[10000];
+int extra_cnt=0;
+
+int stac_count=0;
+
+void reverse(char str[], int len)
+{
+    int start, end;
+    char temp;
+    for(start=0, end=len-1; start < end; start++, end--) {
+        temp = *(str+start);
+        *(str+start) = *(str+end);
+        *(str+end) = temp;
+    }
+}
+
+char* itoa(int num, char* str, int base)
+{
+    int i = 0;
+    int isNegative = 0;
+ 
+  
+    if (num == 0)
+    {
+        str[i++] = '0';
+        str[i] = '\0';
+        return str;
+    }
+
+    if (num < 0 && base == 10)
+    {
+        isNegative = 1;
+        num = -num;
+    }
+ 
+
+    while (num != 0)
+    {
+        int rem = num % base;
+        str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
+        num = num/base;
+    }
+
+    if (isNegative)
+        str[i++] = '-';
+ 
+    str[i] = '\0'; 
+ 
+    reverse(str, i);
+ 
+    return str;
+}
+
+
+
+void putin(char s[100])
+{
+struct stac *temp,*temp1,*pk;
+temp= (struct stac*)malloc(sizeof(struct stac));
+strcpy(temp->ope,"@");
+strcpy(temp->sta,s);
+temp->left=NULL;
+temp->right=NULL;
+temp->updated=0;
+symbol_table *d;	
+	d = take_symbol(temp->sta,nestlvl,func_no,funcCall);
+	if(d!=NULL)
+	{
+	temp->nestl=d->nestingLevel;
+	temp->funcl=d->func_no;
+	}
+stack_list[stac_count++]=temp;
+
+}
+
+void create_tree(int k)
+{
+struct stac* tmp,*pk;
+char var[10];
+char c[100];
+char d[100]="T";
+itoa(n,c,10);
+strcat(d,c);
+if(k==0)
+strcpy(var,"=");
+else if(k==1)
+strcpy(var,"+");
+else if(k==2)
+strcpy(var,"-");
+else if(k==3)
+strcpy(var,"*");
+else if(k==4)
+strcpy(var,"/");
+else if(k==5)
+strcpy(var,"<");
+else if(k==6)
+strcpy(var,">");
+else if(k==7)
+strcpy(var,"<=");
+else if(k==8)
+strcpy(var,"<=");
+else if(k==9)
+strcpy(var,"==");
+else if(k==10)
+strcpy(var,"!=");
+
+if(k!=0)
+	n++;
+
+tmp=(struct stac*)malloc(sizeof(struct stac));
+tmp->right=stack_list[--stac_count];
+tmp->left=stack_list[--stac_count];
+stack_list[stac_count++]=tmp;
+strcpy(tmp->ope,var);
+strcpy(tmp->sta,d);
+	tmp->add=adds;
+	adds++;
+
+if(k!=0)
+	{
+printf("\t%d : %s = %s %s %s\n",tmp->add,tmp->sta,tmp->left->sta,var,tmp->right->sta);
+	if(k>=5)
+	{
+		if(whilecame==1&&ifcame==0&&only_if==0&&forfor==0)
+	        icgwhile(tmp);
+		else if(whilecame==0&&ifcame==1&&only_if==0&&forfor==0)
+			icgif(tmp);
+		else if(whilecame==0&&ifcame==0&&only_if==1&&forfor==0)
+			onlyif(tmp);
+		else if(whilecame==0&&ifcame==0&&forfor==1&&only_if==0)
+			onlyfor(tmp);
+	}
+	}
+else
+printf("\t%d : %s %s %s\n",tmp->add,tmp->left->sta,var,tmp->right->sta);
+}
+
+void icgwhile(struct stac*d)
+{
+	whilopen[z]=adds++;
+    printf("\t%d : IF NOT %s GOTO %s\n",whilopen[z],d->sta,wopen[z]);
+}
+
+void createlabel()
+{
+	z=while_cnt++;
+	char c[100];
+	char b[100];
+	strcpy(b,"L");
+    itoa(f,c,10);
+	f++;
+	strcat(b,c);
+	strcpy(wopen[z],b);
+}
+
+void creategoto()
+{
+	z=--while_cnt;
+	printf("\tGOTO %d\n",whilopen[z]-1);
+	printf("    %s: \n",wopen[z]);
+}
+
+void creategotoif()
+{
+	y=--ifelse_cnt;
+	g=x-1;
+	printf("\tGOTO %s\n",ifafter[g]);
+	printf("    %s : \n",ifelse[y]);
+}
+
+void afterif()
+{
+	printf("    %s: \n",ifafter[--x]);
+}
+
+void createlabelforifafter()
+{
+    char c[100];
+	char b[100];
+	strcpy(b,"L");
+    itoa(f,c,10);
+	f++;
+	strcat(b,c);
+	strcpy(ifafter[x],b);
+	x++;
+}
+
+void createlabelforifelse()
+{
+	y=ifelse_cnt++;
+    char c[100];
+	char b[100];
+	strcpy(b,"L");
+    itoa(f,c,10);
+	f++;
+	strcat(b,c);
+	strcpy(ifelse[y],b);
+}
+
+void icgif(struct stac*d)
+{
+	createlabelforifelse();
+printf("\t%d : IF NOT %s GOTO %s\n",adds++,d->sta,ifelse[y]);
+}
+
+void onlyif(struct stac*d)
+{
+createlabelforonlyif();
+printf("\t%d : IF NOT %s GOTO %s\n",adds++,d->sta,ifonly[y]);
+}
+
+void afteronlyif()
+{
+	if(strcmp(ifonly[y],"")==0)
+	{
+		strcpy(ifonly[y],ifelse[y]);
+	}
+  printf("    %s : \n",ifonly[y]);
+  strcpy(ifonly[y],"");
+  y--;
+  ifelse_cnt--;
+}
+
+void createlabelforonlyif()
+{
+    char c[100];
+	char b[100];
+	strcpy(b,"L");
+    itoa(f,c,10);
+	f++;
+	strcat(b,c);
+	strcpy(ifonly[y],b);
+}
+
+void createfun(char *s)
+{
+printf("\tCALL  %s\n",s);
+}
+
+void onlyfor(struct stac*d)
+{
+forstart=adds;
+createlabelforend();
+printf("\t%d : IF NOT %s GOTO %s\n",adds++,d->sta,forend[h]);
+createlabelforin();
+printf("\tGOTO %s\n",forin);
+createlabelforincrement();
+printf("    %s: \n",forincre);
+}
+
+void createlabelforend()
+{
+    h=fortp++;
+    char c[100];
+	char b[100];
+	strcpy(b,"L");
+    itoa(f,c,10);
+	f++;
+	strcat(b,c);
+	strcpy(forend[h],b);
+}
+
+void createlabelforin()
+{
+    char c[100];
+	char b[100];
+	strcpy(b,"L");
+    itoa(f,c,10);
+	f++;
+	strcat(b,c);
+	strcpy(forin,b);
+}
+
+void createlabelforincrement()
+{
+    char c[100];
+	char b[100];
+	strcpy(b,"L");
+    itoa(f,c,10);
+	f++;
+	strcat(b,c);
+	strcpy(forincre,b);
+}
+
+void gotoforstart()
+{
+	increstart[h]=adds;
+	printf("\tGOTO %d \n",forstart-1);
+	printf("    %s:\n",forin);
+}
+
+void gotoforincre()
+{
+printf("\tGOTO %d \n",increstart[h]-2);
+printf("    %s:\n",forend[h]);
+h--;
+}
---
+++ ICG/symbol_tabel.c
@@ -0,0 +1,177 @@
+
+struct symbol_table
+{
+	char *name;
+	char type[100];
+	char arrDim[100];
+	char list[200];
+	int proDef;
+	int nestingLevel;
+	int is_array;
+	int func_no;
+	int scope_complete;
+	int parameters_count;
+	int typelist[100];
+	struct symbol_table *next;
+};
+
+typedef struct symbol_table symbol_table;
+symbol_table *sym_table = NULL;
+symbol_table *insert_symbol();
+symbol_table *take_symbol();
+symbol_table *print_symbol_table();
+symbol_table *update_scope();
+symbol_table *update_parameters();
+symbol_table *update_datatypes();
+int argscnt();
+
+int argscnt(char *name,int cnt,int templist[10])
+{
+  symbol_table *ptr;  int i;
+  for(ptr=sym_table;ptr!=NULL;ptr=ptr->next)
+   if(strcmp(ptr->name,name)==0 && ptr->proDef==1)
+    if(ptr->parameters_count==cnt)
+     {
+     for(i=0;i<cnt;i++)
+     {
+      if(ptr->typelist[i]!=templist[i] && !((ptr->typelist[i]==3&&templist[i]==4)||(ptr->typelist[i]==4&&templist[i]==3))) //this extra condition when int argument is passed to float then it should accept and vice versa
+        {
+         return -2;
+        }
+     }
+
+     return 1;
+    }
+    else if(ptr->parameters_count>cnt)
+      return 0;
+    else
+      return -1;
+}
+
+
+symbol_table *update_parameters(char *name,int func_no,int Oparameters_count,int templist[10])
+{
+ symbol_table *ptr;
+	for(ptr=sym_table;ptr!=NULL;ptr=ptr->next)
+	 if(ptr->func_no==func_no && ptr->proDef==1)
+     {
+     ptr->parameters_count=Oparameters_count;
+    int i;
+     strcpy(ptr->list,"");
+     for(i=0;i<Oparameters_count;i++)
+     {
+     ptr->typelist[i]=templist[i];
+       switch(templist[i])
+       {
+        case 1:
+			if(i!=Oparameters_count-1)
+             strcat(ptr->list,"void,");
+		    else
+             strcat(ptr->list,"void");
+             break;
+        case 2:
+             if(i!=Oparameters_count-1)
+             strcat(ptr->list,"char,");
+		    else
+             strcat(ptr->list,"char");
+             break;
+        case 3:
+             if(i!=Oparameters_count-1)
+             strcat(ptr->list,"int,");
+		    else
+             strcat(ptr->list,"int");
+             break;
+        case 4:
+            if(i!=Oparameters_count-1)
+             strcat(ptr->list,"float,");
+		    else
+             strcat(ptr->list,"float");
+             break;
+       }
+     }
+  }
+}
+
+symbol_table *print_symbol_table()
+{
+ symbol_table *ptr;
+ FILE *f;
+ char *tmp;
+  f=fopen("output.txt","w");
+ fprintf(f,"Lexeme\tType\tArrayDim  Procedure Definition  Parameters list  nesting level  Belongs to Func \n");
+                  fprintf(f,"----------------------------------------------------------------------------------\n");
+	for(ptr=sym_table;ptr!=NULL;ptr=ptr->next)
+	{
+		if(ptr->proDef==0)
+			tmp="0";
+		else
+			tmp="1";
+		printf("%s \t%s \t%s \t\t%s \t\t\t%d\t\t%d\t\t%s\n",ptr->name,ptr->type,ptr->arrDim,tmp,ptr->nestingLevel,ptr->func_no,ptr->list);
+	fprintf(f,"%s \t\t\t\t%s \t%s \t\t%s \t\t\t%s\t\t%d\t\t%d\n",ptr->name,ptr->type,ptr->arrDim,tmp,ptr->list,ptr->nestingLevel,ptr->func_no);
+	 }
+}
+
+symbol_table *update_scope(int nestlevel)
+{
+symbol_table *ptr;
+ for(ptr=sym_table;ptr!=NULL;ptr=ptr->next)
+   if(ptr->nestingLevel==nestlevel)
+     ptr->scope_complete=1;
+}
+
+symbol_table *insert_symbol(char *sym_name,int sym_type,char *arrDim, char *list,int proDef, int nestlvl,int is_array,int func_no,int func_arg)
+{
+	char type[10];
+	switch(sym_type)
+	{
+		case 1:
+			strcpy(type,"void");
+			break;
+		case 2:
+			strcpy(type,"char");
+			break;
+		case 3:
+			strcpy(type,"int");
+			break;
+		case 4:
+			strcpy(type,"float");
+			break;
+	}
+	symbol_table *ptr;
+	ptr=(symbol_table *)malloc(sizeof(symbol_table));
+	ptr->name=(char *)malloc(strlen(sym_name)+1);
+	strcpy(ptr->name,sym_name);
+	strcpy(ptr->type,type);
+    strcpy(ptr->arrDim,arrDim);
+    strcpy(ptr->list,list);
+    ptr->nestingLevel=nestlvl;
+    ptr->proDef=proDef;
+    ptr->is_array=is_array;
+    ptr->func_no=func_no;
+    ptr->parameters_count=func_arg;
+    ptr->scope_complete=0;
+    ptr->parameters_count=0;
+	ptr->next=(struct symbol_table *)sym_table;
+	sym_table=ptr;
+	return ptr;
+}
+
+symbol_table *take_symbol(char *sym_name,int nestlvl,int func_no,int funcCall)
+{
+	symbol_table *ptr;
+    if(funcCall==1)
+    {
+    for(ptr=sym_table;ptr!=NULL;ptr=ptr->next)
+     if(strcmp(ptr->name,sym_name)==0 && ptr->proDef == 1)
+      return ptr;
+    }
+   else
+	for(ptr=sym_table;ptr!=NULL;ptr=ptr->next)
+	 if(strcmp(ptr->name,sym_name)==0 && nestlvl>=ptr->nestingLevel && ptr->func_no==func_no && ptr->scope_complete!=1)
+	return ptr;
+return 0;
+}
+ 
+
+
+
